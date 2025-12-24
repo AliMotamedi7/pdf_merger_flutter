14ab7cbe82962ac23cf110127388603b6d09429f
ali <visapick.it14@gmail.com>
2025-12-24 10:37:10 +0330

    fix: pub publish issues

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:pdf_merger_flutter/my_pdf_merger.dart';
+import 'package:pdf_merger_flutter/pdf_merger_flutter.dart';
 
 void main() {
   runApp(MyApp());
